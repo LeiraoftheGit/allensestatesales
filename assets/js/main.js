fa71bd8b189e479af9b4fa5cdd594bbb279e57d5
Mitchel Fry <mitch72193@gmail.com>
2024-09-20 14:16:56 -0400

    Add Upcoming sales plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,28 +14,30 @@
   document.addEventListener('scroll', toggleScrolled);
   window.addEventListener('load', toggleScrolled);
 
-  /**
-   * Mobile nav toggle
-   */
-  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+  /** 
+   * Mobile nav menu toggle
+  */
 
-  function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
-  }
-  mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
-
-  /**
-   * Hide mobile nav on same-page/hash links
-   */
-  document.querySelectorAll('#navmenu a').forEach(navmenu => {
-    navmenu.addEventListener('click', () => {
-      if (document.querySelector('.mobile-nav-active')) {
-        mobileNavToogle();
-      }
+   document.addEventListener("DOMContentLoaded", function() {
+    const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+    const navMenu = document.querySelector('.navmenu ul');
+  
+    function mobileNavToggle() {
+      document.querySelector('body').classList.toggle('mobile-nav-active');
+      navMenu.classList.toggle('active');
+      mobileNavToggleBtn.classList.toggle('bi-list');
+      mobileNavToggleBtn.classList.toggle('bi-x');
+    }
+  
+    mobileNavToggleBtn.addEventListener('click', mobileNavToggle);
+  
+    document.querySelectorAll('#navmenu a').forEach(navmenu => {
+      navmenu.addEventListener('click', () => {
+        if (document.querySelector('.mobile-nav-active')) {
+          mobileNavToggle();
+        }
+      });
     });
-
   });
 
   /**
